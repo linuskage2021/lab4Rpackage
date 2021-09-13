745a82248b075f10185c37928b178097a3dcd81d
Linus Kåge <linka360@student.liu.segit config --global --listgit config --global user.email linka360@student.liu.se>
2021-09-13 13:22:52 +0200

    QR regression

---
+++ R/linreg.R
@@ -0,0 +1,46 @@
+linreg <- function(formula, data){
+
+
+  X <- model.matrix(formula, data = data)
+  y <- as.matrix(data[,all.vars(formula)[1], drop = FALSE])
+
+  qr_object <- base::qr(X)
+
+  Q <- base::qr.Q(qr_object)
+  R <- base::qr.R(qr_object)
+
+
+  beta_vec <- solve(R)%*%t(Q)%*%y
+  print(beta_vec)
+
+
+  fitted_values <- X%*%beta_vec
+
+
+  df <- nrow(X) - ncol(X)
+
+  residuals <- y - fitted_values
+
+  residual_variance <- as.numeric((t(residuals) %*% residuals)/df)
+  #dim(residual_variance)
+
+  error_betas <- sqrt(diag(residual_variance*(solve(R) %*% t(solve(R)))))
+
+
+  beta_names <- rownames(beta_vec)
+  beta_vec <- as.vector(beta_vec)
+  names(beta_vec) <- beta_names
+
+  reg_list <- list(beta = beta_vec,
+                   fits = fitted_values,
+                   df = df,
+                   residuals = residuals,
+                   res_var = residual_variance,
+                   error_betas = error_betas,
+                   formula = deparse(substitute(formula)),
+                   data = deparse(substitute(data)))
+
+
+  class(reg_list) <- "linreg"
+  return(reg_list)
+}
